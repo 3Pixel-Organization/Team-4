b97f330c94c998fe41105b6158dd802994d37794
Elias <32110106+lolmanQ@users.noreply.github.com>
2020-09-24 20:59:37 +0200

    player controller and camera controller basic

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public CameraSettings settings;
+
+	public GameObject followObj;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = followObj.transform.position + settings.objOffset;
+		transform.eulerAngles = settings.cameraAngle;
+	}
+}
