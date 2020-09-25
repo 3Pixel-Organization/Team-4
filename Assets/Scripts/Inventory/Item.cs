ced603ecb88f16e32ef58db0ab020198daf6f36f
Elias <32110106+lolmanQ@users.noreply.github.com>
2020-09-25 20:48:59 +0200

    large commit

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+	public string name;
+	public int level;
+	public ItemRarity rarity;
+
+	public Item(string name, int level, ItemRarity rarity)
+	{
+		this.name = name;
+		this.level = level;
+		this.rarity = rarity;
+	}
+}
+
+
+public enum ItemRarity
+{
+	Trash,
+	Normal,
+	Rare,
+	Epic,
+	Legendary,
+	Dev
+}
