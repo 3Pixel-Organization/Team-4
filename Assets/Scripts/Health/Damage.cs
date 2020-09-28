40fe23d5b50fb15ae5991ad106110ceebed7d0e1
Elias <32110106+lolmanQ@users.noreply.github.com>
2020-09-28 19:27:52 +0200

    animations + attack

--- Assets/Scripts/Health/Damage.cs
+++ Assets/Scripts/Health/Damage.cs
@@ -4,20 +4,29 @@ namespace Health
 {
    public class Damage : MonoBehaviour
    {
-      /// <summary>
-      /// Example implementation of Damage Script 
-      /// </summary>
-      [SerializeField]private int damageAmt;
-      private HealthController _healthController;
+	  /// <summary>
+	  /// Example implementation of Damage Script 
+	  /// </summary>
+	  [SerializeField]private int damageAmt;
+	  private HealthController _healthController;
 
-      private void OnCollisionEnter(Collision other)
-      {
-         _healthController = other.gameObject.GetComponent<HealthController>();
-         if (_healthController != null)
-         {
-            _healthController.Damage(damageAmt);
-         }
+	  private void OnCollisionEnter(Collision other)
+	  {
+		 _healthController = other.gameObject.GetComponent<HealthController>();
+		 if (_healthController != null)
+		 {
+			_healthController.Damage(damageAmt);
+		 }
    
-      }
-   }
+	  }
+
+		private void OnTriggerEnter(Collider other)
+		{
+			_healthController = other.gameObject.GetComponent<HealthController>();
+			if (_healthController != null)
+			{
+				_healthController.Damage(damageAmt);
+			}
+		}
+	}
 }
