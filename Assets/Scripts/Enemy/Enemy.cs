4dba2289d7e23ac69544c413bb874b3f0110dfcb
Elias <32110106+lolmanQ@users.noreply.github.com>
2020-10-13 21:13:09 +0200

    content stuff and death

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Health;
 
 public class Enemy : MonoBehaviour
 {
 	public int level;
 	public TMPro.TextMeshProUGUI levelText;
+
+	private HealthController healthController;
 	// Start is called before the first frame update
 	void Start()
 	{
-		
+		healthController = GetComponent<HealthController>();
+		healthController.Death += Death;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		
+	}
+
+	void Death()
+	{
+		if (Random.Range(0, 1) <= 1)
+		{
+			GameManager.current.SpawnLoot(transform.position, ItemManager.CreateArmor("iron Dev", 20, 1000, ItemRarity.Epic, "IronHelmet", 10, new Enchantment(EnchantmentType.None)));
+		}
+		Destroy(this.gameObject);
 	}
 }
