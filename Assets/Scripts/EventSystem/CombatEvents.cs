a7b8f7c9cae2b39b2c73a92070eeab2561c366a3
avin77 <shobhit.7@gmail.com>
2021-06-08 21:11:24 +0530

    Update CombatEvents.cs
    Added DeathEvent though i am not clear right what parameters required but mostly on death we can have the source & target .For now just committing as i didn't know what is the purpose exactly.

--- Assets/Scripts/EventSystem/CombatEvents.cs
+++ Assets/Scripts/EventSystem/CombatEvents.cs
@@ -12,9 +12,16 @@ namespace EventSystem
 	{
 		public delegate void AttackEventDel(GameObject target, GameObject source, Attack attack, AttackResponse attackResponse);
 		public event AttackEventDel OnAttack;
+
+		public event Action<GameObject, GameObject, Attack, AttackResponse> OnDeath;
 		public void AttackEvent(GameObject target, GameObject source, Attack attack, AttackResponse attackResponse)
 		{
 			OnAttack?.Invoke(target, source, attack, attackResponse);
+		}
+
+		public void DeathEvent(GameObject target, GameObject source, Attack attack, AttackResponse attackResponse)
+		{
+			OnDeath?.Invoke(target, source, attack, attackResponse);
 		}
 	}
 }
