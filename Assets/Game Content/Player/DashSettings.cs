00880029f526a685806a5f158f62a16f9118edfc
Elias <32110106+lolmanQ@users.noreply.github.com>
2021-11-30 14:56:57 +0100

    Folder restructure

---
+++ Assets/Game Content/Player/DashSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Settings/Dash settings")]
+public class DashSettings : ScriptableObject
+{
+	public float duration;
+	public float distance;
+	public float speed;
+	public float cooldown;
+}
--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,5 +1,4 @@
-﻿using BBUnity.Actions;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
